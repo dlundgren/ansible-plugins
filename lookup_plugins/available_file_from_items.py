9aec629f339570fe98eda19689ff61cd05074807
David Lundgren <dlundgren@syberisle.net>
2016-07-15 10:19:25 -0500

    Added file comment

--- lookup_plugins/available_file_from_items.py
+++ lookup_plugins/available_file_from_items.py
@@ -1,10 +1,27 @@
-# @todo COMMENT THIS MO ^ dlundgren
+# Ansible lookup plugin for getting the first available file given a list of items and a template
+# (c) 2015,2016 David Lundgren <dlundgren@syberisle.net>
+#
+# Given a list of items it will attempt to find a file in the regular list of paths that is similar to the name
+#
+# - name: Add SSH keys
+#  authorized_key:
+#    user: "{{ item.username }}"
+#    state: present
+#    key: "{{ lookup('file', item.pubkey) }} }}"
+#    manage_dir: yes
+#    path: '/home/{{ item.username }}/.ssh/authorized_keys'
+#  with_available_file_from_items:
+#    items: "{{ users }}"
+#    name: files/ssh/keys/{{ item.username }}.pubkeys
+#    key: pubkey
+#
+# This will look in the {role}/files/ssh/keys/, {playbook}/files/ssh/keys/ folders for the {username}.pubkeys file.
+# If the file is not found then the user is not returned in the list of items to be used.
 
 import os
 
 from ansible import utils
 from ansible import constants as C
-from ansible.errors import AnsibleError
 from ansible.utils import template
 from ansible.plugins.lookup import LookupBase
 
